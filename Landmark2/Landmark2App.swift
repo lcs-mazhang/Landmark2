55b4f9e6e8c21847e38f3a0a1910400d45421934
Matthew Zhang <gzhang@lcs.on.ca>
2024-02-09 11:08:28 -0500

    Initial Commit

---
+++ Landmark2/Landmark2App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark2App.swift
+//  Landmark2
+//
+//  Created by GengYu Zhang on 2024-02-09.
+//
+
+import SwiftUI
+
+@main
+struct Landmark2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
